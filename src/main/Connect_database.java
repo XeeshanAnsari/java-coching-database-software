7ff4d5c2dee3c65f7284e5a6a7fa54fcd9223150
xeeshan <xeeshan@mail.com>
2016-02-28 18:15:11 -0800

    Add a dataConnection Class for connect to database And add Addmition_page

---
+++ src/main/Connect_database.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Xeeshan
+ */
+public class Connect_database {
+    Connection conn=null;
+    public static Connection connecerDb(){
+        try {
+            Class.forName("org.sqlite.JDBC");
+            Connection conn=DriverManager.getConnection("jdbc:sqlite:C:\\Users\\Xeeshan\\Documents\\NetBeansProjects\\Coching_Database\\database.sqlite");
+            JOptionPane.showMessageDialog(null, "connection ");
+            return conn;
+            
+        } catch (Exception e) {
+          JOptionPane.showMessageDialog(null, e);
+          return null;
+        }
+        
+    }
+    
+}
